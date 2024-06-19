ca886c8ea4356a20755916f6583d85d4098cbdb5
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-2.compute.internal>
2024-06-19 00:27:54 +0000

    fix : package 수정 및 capsule 기능 수정

--- src/views/CompleteTCapsuleDetail.vue
+++ src/views/CompleteTCapsuleDetail.vue
@@ -142,9 +142,9 @@ const TCapsuleDetails = () => {
   .then(response => {
     console.log(response.data);
     capsuleDetail.value = response.data;
-    imagePath.value = response.data.imagePath ? response.data.imagePath.map(imagePath => { return `http://13.125.169.9:3000/${imagePath}` }) : [];
-    videoPath.value = response.data.videoPath ? `http://13.125.169.9:3000/${response.data.videoPath}` : '';
-    audioPath.value = response.data.audioPath ? `http://13.125.169.9:3000/${response.data.audioPath}` : '';
+    imagePath.value = response.data.imagePath ? response.data.imagePath.map(imagePath => { return imagePath }) : [];
+    videoPath.value = response.data.videoPath ? response.data.videoPath : '';
+    audioPath.value = response.data.audioPath ? response.data.audioPath : '';
   })
   .catch(error => {
     console.error(error);
